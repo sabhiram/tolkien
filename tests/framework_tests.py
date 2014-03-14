3cd126d4da1169410531f078135eb395cbb25204
Shaba Abhiram <shabarivas@gmail.com>
2014-03-14 14:40:30 -0700

    Minor test fixes, removing un-needed params from mockplugin

--- tests/framework_tests.py
+++ tests/framework_tests.py
@@ -4,6 +4,9 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
 import tolkien
 
+"""
+    Base tests for framework
+"""
 class TestTolkienFramework(ULT.TestCase):
     
     def setUp(self):
@@ -17,19 +20,24 @@ class TestTolkienFramework(ULT.TestCase):
     def test_test(self):
         self.assertEqual(0, 0)
 
-# Mock plugin for the filter test
-class MockPlugin:
-    def __init__(self, n, d, g):
-        self.name = n
-        self.project_dir_name = d
-        self.git_repo = g
 
+"""
+    Mock plugin for the filter test (only relevant arg is the name)
+"""
+class MockPlugin:
+    def __init__(self, n):
+        self.name = n
+
+
+"""
+    Test the filtering of plugins
+"""
 class TestPluginFilter(ULT.TestCase):
     def setUp(self):
         self.plugins = []
-        self.plugins.append(MockPlugin("foo", "foo", "foo"))
-        self.plugins.append(MockPlugin("bar", "bar", "bar"))
-        self.plugins.append(MockPlugin("baz", "baz", "baz"))
+        self.plugins.append(MockPlugin("foo"))
+        self.plugins.append(MockPlugin("bar"))
+        self.plugins.append(MockPlugin("baz"))
         return
 
     def tearDown(self):
@@ -43,9 +51,6 @@ class TestPluginFilter(ULT.TestCase):
     def test_filter_multiple(self):
         result = list(tolkien.filter_plugins(self.plugins, ["fOo", "BaR"]))
         self.assertEqual(len(result), 2)
-        
-
-
 
 if __name__ == "__main__":
     ULT.main()
